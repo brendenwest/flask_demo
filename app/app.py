5010f6609304f57a745b5fe1d885a2a7d7833be7
Brenden West <brendenw@hotmail.com>
2020-06-02 19:42:55 -0700

    add post route

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
    return 'Ahoy there matey'
+
+@app.route('/predict', methods=['POST'])
+def make_prediction():
+   data = request.get_json()
+   return "you posted {0}".format(data['message'])
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
